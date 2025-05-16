6eae9ca1070f6b557d109bfd112741d0cc2b215a
elitvinov <yevhen.litvinov@nixs.com>
2025-05-16 16:00:41 +0200

    Added new package with class for "TypeConversion" topic with finished practice tasks. Please ignore MyOwnAdditionalPractice class - it is only for me (and may be it is better not to push it here at all).

--- src/main/java/org/example/MyOwnAdditionalPractice.java
+++ src/main/java/org/example/MyOwnAdditionalPractice.java
@@ -1,28 +1,6 @@
 package org.example;
-/*
-✍️ Завдання:
-Створи метод changeNumber(int x) — змінює x на 100
-Створи метод changeArray(int[] arr) — змінює перший елемент масиву на 100
-Виведи значення x і arr[0] після виклику методів
- */
-
 
 public class MyOwnAdditionalPractice {
-    public static void changeNumber(int x) {
-        x = 100;
-    }
 
-    public static void changeArray(int[] arr) {
-        arr[0] = 100;
-    }
 
-    public static void main(String[] args) {
-        int x = 5;
-        int[] arr = {1, 2, 3};
-        changeArray(arr);
-        changeNumber(x);
-
-        System.out.println("x = " + x);
-        System.out.println("arr[0] = " + arr[0]);
-    }
 }
