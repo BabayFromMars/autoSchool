ff70026bdf8db2a032c720262e2ed09b2fcd6599
elitvinov <yevhen.litvinov@nixs.com>
2025-06-06 13:24:31 +0200

    Finished Collections practical task.

---
+++ src/main/java/collections/Cat.java
@@ -0,0 +1,11 @@
+package collections;
+
+public class Cat {
+
+    public Cat() {
+    }
+
+    public String toString() {
+        return "Cat";
+    }
+}
--- src/main/java/org/example/MyOwnAdditionalPractice.java
+++ src/main/java/org/example/MyOwnAdditionalPractice.java
@@ -2,5 +2,6 @@ package org.example;
 
 public class MyOwnAdditionalPractice {
 
+    int[][] name = {{1, 2, 3}, {1, 2, 2}};
 
 }
