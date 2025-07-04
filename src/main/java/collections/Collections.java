4bd0b3b371002eed5051176de8d15aa7f092184c
elitvinov <yevhen.litvinov@nixs.com>
2025-07-04 17:55:31 +0200

    Fixed mistakes for Collections tasks.

--- src/main/java/collections/Collections.java
+++ src/main/java/collections/Collections.java
@@ -5,23 +5,23 @@ import java.util.*;
 
 public class Collections extends Cat {
     public static void main(String[] args) {
-//        task1();
-//        task2();
-        //       task3();
-//        task4();
-//        task5();
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
         task6();
-//        task7();
-//        task8();
-//        task9();
-//        task10();
-//        task11();
-//        task12();
-//        task13();
-//        task14();
-//        task15();
-//        task16();
-//        task17();
+        task7();
+        task8();
+        task9();
+        task10();
+        task11();
+        task12();
+        task13();
+        task14();
+        task15();
+        task16();
+        task17();
     }
 
     /*Task 1.  HashSet from plants
